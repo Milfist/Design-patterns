0f0ca5bf4d4d329b466988e2eefb5a865ab40c0a
Miguel Anguita <miguel.anguita@soprasteria.com>
2018-02-01 16:37:51 +0100

    Solution

--- Creational/patterns_2_builder_ejercicio/src/org/formacion/builder/Persona.java
+++ Creational/patterns_2_builder_ejercicio/src/org/formacion/builder/Persona.java
@@ -24,20 +24,16 @@ public class Persona {
 			return this;
 		}
 		
-		public Builder setMayor(int edad, String lugarTrabajo) {
+		public BuilderMayor setMayor(int edad) {
 			if (edad < 18) throw new IllegalArgumentException("es menor de edad " + edad);
 			persona.edad = edad;
-			persona.lugarTrabajo = lugarTrabajo;
-			persona.colegio = null;
-			return this;
+			return new Persona.BuilderMayor(persona);
 		}
 		
-		public Builder setMenor(int edad, String colegio) {
+		public BuilderMenor setMenor(int edad) {
 			if (edad >= 18) throw new IllegalArgumentException("es mayor de edad " + edad);
 			persona.edad = edad;
-			persona.colegio = colegio;
-			persona.lugarTrabajo = null;
-			return this;
+			return new Persona.BuilderMenor(persona);
 		}
 	
 		public Persona build() {
@@ -46,4 +42,37 @@ public class Persona {
 
 	}
 	
+	public static class BuilderMayor {
+		private Persona persona;
+
+		public BuilderMayor(Persona persona) {
+			this.persona = persona;
+		}
+		
+		public BuilderMayor setLugarTrabajo(String trabajo) {
+			persona.lugarTrabajo = trabajo;
+			return this;
+		}
+		
+		public Persona build() {
+			return persona;
+		}
+	}
+	
+	public static class BuilderMenor {
+		private Persona persona;
+
+		public BuilderMenor(Persona persona) {
+			this.persona = persona;
+		}
+		
+		public BuilderMenor setColegio(String colegio) {
+			persona.colegio = colegio;
+			return this;
+		}
+		
+		public Persona build() {
+			return persona;
+		}
+	}
 }
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Design-patterns
 Pattern application exercises
+
+
+
+
+
+![alt text](https://github.com/Milfist/Docs/blob/master/milfist.JPG)
