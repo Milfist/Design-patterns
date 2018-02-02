0fa53c3388e51eca78b24d0675864afb66b2843d
Miguel Anguita <miguel.anguita@soprasteria.com>
2018-02-02 10:23:34 +0100

    Factory Method Pattern
    Exercise for the application of the pattern
    
    Co-Authored-By: milfist <milfist@users.noreply.github.com>

---
+++ Creational/patterns_3_factory_method_ejercicio/test/org/formacion/factorymethod/TestLavadora.java
@@ -0,0 +1,35 @@
+package org.formacion.factorymethod;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestLavadora {
+
+	@Test
+	public void test_carga_frontal() {
+		
+		LavadoraFactory lavadoraFactory = new LavadoraCargaFrontalFactory();
+		Lavadora lavadora = lavadoraFactory.crea();
+		lavadora.ponerMandos();
+		lavadora.ponerTambor();
+		
+		assertEquals("frontal", lavadora.tipoCarga);
+		assertTrue(lavadora.tieneMandos);
+		assertTrue(lavadora.tieneTambor);
+	}
+
+	@Test
+	public void test_carga_superior() {
+		
+		LavadoraFactory lavadoraFactory = new LavadoraCargaSuperiorFactory();
+		Lavadora lavadora = lavadoraFactory.crea();
+		lavadora.ponerMandos();
+		lavadora.ponerTambor();
+		
+		assertEquals("superior", lavadora.tipoCarga);
+		assertTrue(lavadora.tieneMandos);
+		assertTrue(lavadora.tieneTambor);
+	}
+
+}
