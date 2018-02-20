0ec53bb6d19901bf0645fbd82041ca8886bd67db
Miguel Anguita <miguel.anguita@soprasteria.com>
2018-02-20 08:30:05 +0100

    State Pattern (null object)
    Pattern application exercise
    
    Co-Authored-By: milfist <milfist@users.noreply.github.com>

---
+++ Behavioral/patterns_24_state__null_object/src/org/milfist/nullObject/AbstractCustomer.java
@@ -0,0 +1,11 @@
+package org.milfist.nullObject;
+
+public abstract class AbstractCustomer {
+	
+	protected String name;
+
+	public abstract boolean isNil();
+
+	public abstract String getName();
+	
+}
